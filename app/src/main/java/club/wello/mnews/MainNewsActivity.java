48c8cbc137a9b2cac22ea2ec2b70f7c28a936896
ma-weihao <hellowello1996@outlook.com>
2017-07-30 22:35:02 +0800

    First Commit

---
+++ app/src/main/java/club/wello/mnews/MainNewsActivity.java
@@ -0,0 +1,13 @@
+package club.wello.mnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_news);
+    }
+}
